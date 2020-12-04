795b3131a092c1e6f3dbec2cbe4fa44b883ce751
Gunnar Morling <gunnar.morling@googlemail.com>
2020-12-04 12:13:48 +0100

    WIP

---
+++ connect/1.3/copy-assemblies.sh
@@ -0,0 +1,9 @@
+DEBEZIUM_VERSION=1.3.0-SNAPSHOT
+DEBEZIUM_WORKDIR=/Users/gunnar/Development/debezium/_detached_build/debezium
+DEBEZIUM_INCUBATOR_WORKDIR=/Users/gunnar/Development/debezium/_detached_build/debezium-incubator
+
+# cp $DEBEZIUM_WORKDIR/debezium-connector-postgres/target/debezium-connector-postgres-$DEBEZIUM_VERSION-plugin.tar.gz .
+# cp $DEBEZIUM_WORKDIR/debezium-connector-mongodb/target/debezium-connector-mongodb-$DEBEZIUM_VERSION-plugin.tar.gz .
+# cp $DEBEZIUM_WORKDIR/debezium-connector-mysql/target/debezium-connector-mysql-$DEBEZIUM_VERSION-plugin.tar.gz .
+# cp $DEBEZIUM_INCUBATOR_WORKDIR/debezium-connector-oracle/target/debezium-connector-oracle-$DEBEZIUM_VERSION-plugin.tar.gz .
+cp $DEBEZIUM_WORKDIR/debezium-connector-sqlserver/target/debezium-connector-sqlserver-$DEBEZIUM_VERSION-plugin.tar.gz .
---
+++ connect/1.6/copy-assemblies.sh
@@ -0,0 +1,8 @@
+DEBEZIUM_VERSION=1.6.0-SNAPSHOT
+DEBEZIUM_WORKDIR=/Users/gunnar/Development/debezium/_detached_build/debezium
+
+cp $DEBEZIUM_WORKDIR/debezium-connector-postgres/target/debezium-connector-postgres-$DEBEZIUM_VERSION-plugin.tar.gz .
+# cp $DEBEZIUM_WORKDIR/debezium-connector-mongodb/target/debezium-connector-mongodb-$DEBEZIUM_VERSION-plugin.tar.gz .
+# cp $DEBEZIUM_WORKDIR/debezium-connector-mysql/target/debezium-connector-mysql-$DEBEZIUM_VERSION-plugin.tar.gz .
+# cp $DEBEZIUM_INCUBATOR_WORKDIR/debezium-connector-oracle/target/debezium-connector-oracle-$DEBEZIUM_VERSION-plugin.tar.gz .
+# cp $DEBEZIUM_WORKDIR/debezium-connector-sqlserver/target/debezium-connector-sqlserver-$DEBEZIUM_VERSION-plugin.tar.gz .
