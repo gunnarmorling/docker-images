cbd322d8a1f262be8bc48500f1a0776f835e6e3d
Gunnar Morling <gunnar.morling@googlemail.com>
2021-05-07 18:40:26 +0200

    DBZ-3444 Upgrading to AK 2.8; basic support for KRaft mode

--- kafka/1.6/docker-entrypoint.sh
+++ kafka/1.6/docker-entrypoint.sh
@@ -7,6 +7,15 @@ if [[ -z "$BROKER_ID" ]]; then
     BROKER_ID=1
     echo "WARNING: Using default BROKER_ID=1, which is valid only for non-clustered installations."
 fi
+
+if [[ -z "$CLUSTER_ID" ]]; then
+    echo "Starting in ZooKeeper mode."
+    CONFIG_FILE=config/server.properties
+else
+    echo "Starting in KRaft mode."
+    CONFIG_FILE=config/kraft/server.properties
+fi
+
 if [[ -z "$ZOOKEEPER_CONNECT" ]]; then
     # Look for any environment variables set by Docker container linking. For example, if the container
     # running Zookeeper were named 'zoo' in this container, then Docker should have created several envs,
@@ -24,7 +33,7 @@ if [[ -n "$HEAP_OPTS" ]]; then
 fi
 
 export KAFKA_ZOOKEEPER_CONNECT=$ZOOKEEPER_CONNECT
-export KAFKA_BROKER_ID=$BROKER_ID
+export KAFKA_NODE_ID=$BROKER_ID
 export KAFKA_LOG_DIRS="$KAFKA_HOME/data/$KAFKA_BROKER_ID"
 mkdir -p $KAFKA_LOG_DIRS
 unset BROKER_ID
@@ -39,6 +48,7 @@ fi
 
 : ${PORT:=9092}
 : ${ADVERTISED_PORT:=9092}
+: ${CONTROLLER_PORT:=9093}
 
 : ${ADVERTISED_PORT:=${PORT}}
 : ${ADVERTISED_HOST_NAME:=${HOST_NAME}}
@@ -49,11 +59,17 @@ fi
 : ${KAFKA_PORT:=${PORT}}
 : ${KAFKA_HOST_NAME:=${HOST_NAME}}
 
-: ${KAFKA_LISTENERS:=PLAINTEXT://$KAFKA_HOST_NAME:$KAFKA_PORT}
+if [[ -z "$CLUSTER_ID" ]]; then
+    : ${KAFKA_LISTENERS:=PLAINTEXT://$KAFKA_HOST_NAME:$KAFKA_PORT}
+else
+    echo "Starting in KRaft mode."
+    : ${KAFKA_LISTENERS:=PLAINTEXT://$KAFKA_HOST_NAME:$KAFKA_PORT,CONTROLLER://$KAFKA_HOST_NAME:$CONTROLLER_PORT}
+fi
+
 : ${KAFKA_ADVERTISED_LISTENERS:=PLAINTEXT://$KAFKA_ADVERTISED_HOST_NAME:$KAFKA_ADVERTISED_PORT}
 
 export KAFKA_LISTENERS KAFKA_ADVERTISED_LISTENERS
-unset HOST_NAME ADVERTISED_HOST_NAME KAFKA_HOST_NAME KAFKA_ADVERTISED_HOST_NAME PORT ADVERTISED_PORT KAFKA_PORT KAFKA_ADVERTISED_PORT
+unset HOST_NAME ADVERTISED_HOST_NAME KAFKA_HOST_NAME KAFKA_ADVERTISED_HOST_NAME PORT ADVERTISED_PORT KAFKA_PORT KAFKA_ADVERTISED_PORT CONTROLLER_PORT
 
 echo "Using KAFKA_LISTENERS=$KAFKA_LISTENERS and KAFKA_ADVERTISED_LISTENERS=$KAFKA_ADVERTISED_LISTENERS"
 
@@ -86,7 +102,7 @@ case $1 in
         unset LOG_LEVEL
 
         # Add missing EOF at the end of the config file
-        echo "" >> $KAFKA_HOME/config/server.properties
+        echo "" >> $KAFKA_HOME/$CONFIG_FILE
 
         #
         # Process all environment variables that start with 'KAFKA_' (but not 'KAFKA_HOME' or 'KAFKA_VERSION'):
@@ -96,12 +112,12 @@ case $1 in
           env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
           if [[ $env_var =~ ^KAFKA_ && $env_var != "KAFKA_VERSION" && $env_var != "KAFKA_HOME"  && $env_var != "KAFKA_LOG4J_OPTS" && $env_var != "KAFKA_JMX_OPTS" ]]; then
             prop_name=`echo "$VAR" | sed -r "s/^KAFKA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
-            if egrep -q "(^|^#)$prop_name=" $KAFKA_HOME/config/server.properties; then
+            if egrep -q "(^|^#)$prop_name=" $KAFKA_HOME/$CONFIG_FILE; then
                 #note that no config names or values may contain an '@' char
-                sed -r -i "s@(^|^#)($prop_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties
+                sed -r -i "s%(^|^#)($prop_name)=(.*)%\2=${!env_var}%g" $KAFKA_HOME/$CONFIG_FILE
             else
                 #echo "Adding property $prop_name=${!env_var}"
-                echo "$prop_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
+                echo "$prop_name=${!env_var}" >> $KAFKA_HOME/$CONFIG_FILE
             fi
           fi
         done
@@ -127,7 +143,14 @@ case $1 in
                 done
             )&
         fi
-        exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+
+        if [[ ! -z "$CLUSTER_ID" && ! -f "$KAFKA_LOG_DIRS/meta.properties" ]]; then
+                echo "No meta.properties found in $KAFKA_LOG_DIRS; going to format the directory"
+
+                $KAFKA_HOME/bin/kafka-storage.sh format -t $CLUSTER_ID -c $KAFKA_HOME/$CONFIG_FILE
+        fi
+
+        exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/$CONFIG_FILE
         ;;
     watch-topic)
         shift
